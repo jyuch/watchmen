cd5ca19bce0c8a630fdb5b7f6fc632fdf27b42e9
jyuch <jyuch@users.noreply.github.com>
2023-01-09 21:02:00 +0900

    Remove description from disk

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 #[cfg(windows)]
 fn main() -> std::io::Result<()> {
     let mut res = winres::WindowsResource::new();
-    res.set("FileDescription", env!("CARGO_PKG_DESCRIPTION"));
     res.set_icon("media/icon.ico");
     res.compile()
 }
